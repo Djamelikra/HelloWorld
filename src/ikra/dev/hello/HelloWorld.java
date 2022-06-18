4b1332b88a5b31a7b41dc0ee326816770e87a604
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-06-18 18:05:19 +0200

    class + img md

--- src/ikra/dev/hello/HelloWorld.java
+++ src/ikra/dev/hello/HelloWorld.java
@@ -2,6 +2,15 @@ package ikra.dev.hello;
 
 public class HelloWorld {
 	
-	
+	 public void welcome() {
+		    System.out.print("Hello World!");
+		  }
 
+	 public static void main(String[] args) {
+		 HelloWorld hw = new HelloWorld();
+		 hw.welcome();
+	}
+	 
 }
+
+
