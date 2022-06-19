641a6a2f95173f53481e57fadeda386cccf73e85
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-06-19 13:00:50 +0200

    dialogs

--- src/ikra/dev/controlIf/ControlIf.java
+++ src/ikra/dev/controlIf/ControlIf.java
@@ -1,8 +1,13 @@
 package ikra.dev.controlIf;
 
+import java.awt.Toolkit;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
+
 
 public class ControlIf {
 
@@ -14,9 +19,19 @@ public class ControlIf {
 
 	}
 
+	public void tryIf() {
+		Toolkit.getDefaultToolkit().beep();
+		String strInput = JOptionPane.showInputDialog(null, "Enter a number");
+		int nbr1 = Integer.parseInt(strInput);
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null,"Message", "Title", 1);
+
+	}
+
 	public static void main(String[] args) throws IOException {
 		ControlIf cif = new ControlIf();
-		cif.testIf();
+//		cif.testIf();
+		cif.tryIf();
 
 	}
 
